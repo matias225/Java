acac735410604e9165a3ac7898995d0245e38b1f
Matías Romani <33184522+matias225@users.noreply.github.com>
2026-09-14 22:50:36 -0300

    Agregado IntroduccionJava

---
+++ IntroduccionJava/src/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+    // Agregamos el metodo principal (main)
+    static void main(String[] args) {
+        System.out.println("Hola mundo desde Java");
+    }
+}
---
+++ IntroduccionJava/src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    static void main(String[] args) {
+        System.out.println("Nombre: Matias Nicolas Romani");
+        System.out.println("Edad: 33 años");
+        System.out.println("País: Argentina");
+    }
+}
